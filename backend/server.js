67e17807e97ea9be6c1d51f224e021a8424e628a
Viktor Jankuloski <viktor.jankuloski@hotmail.com>
2023-09-24 23:58:04 +0200

    implemented user email and password validation, created auth middleware and a jwt http only cookie

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-const port = process.env.PORT || 5000;
+dotenv.config();
 
 connectDB();
 
+const port = process.env.PORT || 5000;
+
 const app = express();
+
+// Body parser middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
